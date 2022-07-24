2e4183eccc7f87b08cb8720b41202f147dd15d40
Jamesb13579 <jamesbagge@ymail.com>
2022-07-24 22:55:38 +0000

    new

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://wavmmmpgpgixns:f2483b5952e5616a2b1be9e6d0b8c9fb66ecba562377f3a1b1dbe201a2eba1d5@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/d8g8oto5kcgdfq"
 os.environ["SECRET_KEY"] = "Curren12"
-os.environ["CLOUDINARY_URL"] = "CLOUDINARY_URL=cloudinary://544614571313615:CG7kzrKuGtb8r4k6LK4gy_ICmJA@jamesbcloud"
+os.environ["CLOUDINARY_URL"] = "cloudinary://544614571313615:CG7kzrKuGtb8r4k6LK4gy_ICmJA@jamesbcloud"
